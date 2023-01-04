02120f770493346ec71750a685246b0146d59386
Akanksha Agrawal <akanksha.agrawal@kalvium.community>
2023-01-05 01:01:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
-
+import axios from 'axios';
+// 
 function App() {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: "whatever-you-want",
+      },
+    };
+
+    const url = "https://reactnd-books-api.udacity.com/books";
+
+    axios
+      .get(url, config)
+      .then((res) => {
+        setData(res.data.books);
+      })
+      .catch(err=>{
+            console.log("Status Code: "+err.response.status)
+            if(err.response.status===404){
+              console.log("Website not found")
+            }
+            else{
+              console.log(err)
+            }
+          })
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((item)=>{
+        return(
+          <div key={item.id}>
+            <h4>{item.title}</h4>
+            <div className='flex'>
+              <img src={item.imageLinks.smallThumbnail} alt=""></img>
+              <p>{item.description}</p>
+            </div>
+            {item.authors.map((author,index)=>{
+              return <span key={index}>{author}</span>
+            })}
+            <hr></hr>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
